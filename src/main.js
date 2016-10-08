a9051817121f28f1dfe2f6c3c85386e2800f51d8
Giles Edkins <edkins@gmail.com>
2016-10-08 00:03:21 -0400

    Serve static files
    Use httpdispatcher

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 var http = require('http');
+var dispatcher = require('httpdispatcher');
 
 const port = 8080;
 
 function handleRequest( request, response )
 {
-	response.end( 'Stuff' );
+	dispatcher.dispatch( request, response );
 }
+dispatcher.onGet("/", function(req,res) {
+	req.url = "/resources/index.html";
+	dispatcher.staticListener(req,res);
+});
+dispatcher.onGet("/page1", function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Page One');
+});	
+dispatcher.setStatic( 'resources' );
+dispatcher.setStaticDirname( '' );
 
 var server = http.createServer( handleRequest );
 
